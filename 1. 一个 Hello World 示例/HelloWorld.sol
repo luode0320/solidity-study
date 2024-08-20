3ad69fe6a299f9f6b65bbd5ca896f15ff5d398f3
luode0320 <1846555387@qq.com>
2024-08-21 04:50:18 +0800

    简单的sol

---
+++ 1. 一个 Hello World 示例/HelloWorld.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Solidity版本声明
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    // 网络:  事件声明
+    event Log(address indexed sender, string message);
+
+    // 构造的功能
+    address owner;
+
+    // 构造的功能
+    constructor() {
+        owner = msg.sender; // 设置的合同部署人员作为所有者
+    }
+
+    // 发出Log事件的外部函数
+    function test() external {
+        emit Log(msg.sender, "Hello EVM!"); // 发送带有发件人地址和消息的日志事件
+    }
+
+    // 外部视图功能，用于获取问候信息
+    function get() external view returns (string memory greeting) {
+        if (owner == msg.sender) {
+            return greeting = "Hello, world!"; // 如果来电者是主人，则返回问候信息
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## HelloWorld
+
+这是一个solidity的案例库
